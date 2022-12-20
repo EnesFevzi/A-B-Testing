6dfbe1a196708e9678991e562faf14375018cf27
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2022-12-20 11:18:14 +0300

    Add files via upload

---
+++ AB_Testing.py
@@ -0,0 +1,137 @@
+# impression: Reklam görüntüleme sayısı
+# Click: Görüntülenen reklama tıklama sayısı
+# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
+# Earning: Satın alınan ürünler sonrası elde edilen kazanç
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import shapiro, levene, ttest_ind
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+dataframe_control = pd.read_excel("C:/Users/Enes Fevzi/Desktop/measurement_problems-220803-201851/Case 1/ABTesti-221116-002840/ABTesti/ab_testing.xlsx" , sheet_name="Control Group")
+dataframe_test = pd.read_excel("C:/Users/Enes Fevzi/Desktop/measurement_problems-220803-201851/Case 1/ABTesti-221116-002840/ABTesti/ab_testing.xlsx" , sheet_name="Control Group")
+
+
+df_control = dataframe_control.copy()
+df_test = dataframe_test.copy()
+
+
+# Adım 2: Kontrol ve test grubu verilerini analizi
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head())
+    print("##################### Tail #####################")
+    print(dataframe.tail())
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df_control)
+check_df(df_test)
+
+#Analiz işleminden sonra concat metodunu kullanarak kontrol ve test grubu verilerini birleştirelim.
+
+df_control["group"] = "control"
+df_test["group"] = "test"
+
+df = pd.concat([df_control,df_test], axis=0,ignore_index=False)
+df.head()
+df.tail()
+
+#####################################################
+#A/B Testinin Hipotezinin Tanımlanması
+#####################################################
+
+# Hipotezi tanımlayınız.
+
+# H0 : M1 = M2 (Kontrol grubu ve test grubu satın alma ortalamaları arasında fark yoktur.)
+# H1 : M1!= M2 (Kontrol grubu ve test grubu satın alma ortalamaları arasında fark vardır.)
+
+
+df.groupby("group").agg({"Purchase":"mean"})
+
+
+#  Hipotez testi yapılmadan önce varsayım kontrollerini yapalım.
+
+# Kontrol ve test grubunun normallik varsayımına uyup uymadığını Purchase değişkeni üzerinden ayrı ayrı test ediniz
+# Normallik Varsayımı :
+# H0: Normal dağılım varsayımı sağlanmaktadır.
+# H1: Normal dağılım varsayımı sağlanmamaktadır
+# p < 0.05 H0 RED
+# p > 0.05 H0 REDDEDİLEMEZ
+# Test sonucuna göre normallik varsayımı kontrol ve test grupları için sağlanıyor mu ?
+# Elde edilen p-valuedeğerlerini yorumlayınız.
+
+
+test_stat, pvalue = shapiro(df.loc[df["group"] == "control", "Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+# p-value=0.5891
+# HO reddedilemez. Control grubunun değerleri normal dağılım varsayımını sağlamaktadır.
+
+
+test_stat, pvalue = shapiro(df.loc[df["group"] == "test", "Purchase"])
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+# Varyans Homojenliği :
+# H0: Varyanslarhomojendir.
+# H1: Varyanslarhomojen Değildir.
+# p < 0.05 H0 RED
+# p > 0.05 H0 REDDEDİLEMEZ
+# Kontrol ve test grubu için varyans homojenliğinin sağlanıp sağlanmadığını Purchase değişkeni üzerinden test ediniz.
+# Test sonucuna göre normallik varsayımı sağlanıyor mu? Elde edilen p-valuedeğerlerini yorumlayınız.
+
+test_stat, pvalue = levene(df.loc[df["group"] == "control", "Purchase"].dropna(),
+                           df.loc[df["group"] == "test", "Purchase"].dropna())
+
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+
+############################
+#Varsayımlar sağlanıyorsa bağımsız iki örneklem t testi (parametrik test)
+############################
+
+test_stat, pvalue = ttest_ind(df.loc[df["group"] == "control", "Purchase"],
+                              df.loc[df["group"] == "test", "Purchase"],
+                              equal_var=True)
+
+print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+
+# p-value < ise 0.05 'ten HO RED.
+# p-value < değilse 0.05 H0 REDDEDILEMEZ.
+
+# Adım 3: Test sonucunda elde edilen p_valuedeğerini göz önünde bulundurarak kontrol ve test grubu satın alma
+# ortalamaları arasında istatistiki olarak anlamlı bir fark olup olmadığını yorumlayınız.
+
+# p-value=0.3493
+# HO reddedilemez. Kontrol ve test grubu satın alma ortalamaları arasında istatistiksel olarak anlamlı farklılık yoktur.
+
+
+##############################################################
+#Sonuçların Analizi
+##############################################################
+
+# Adım 1: Hangi testi kullandınız, sebeplerini belirtiniz.
+
+#İki gruba da ilk önce normallik varsayımı testi yapılmıştır.
+#H0 reddilemediği için Varyans homojenliği uygulanmış ve sonuca göre varyans homojenliği sağlandığından iki gruba da T testi uygunlanmıştır.
+#Test ve Kontrol gruplarının ortalamaları arasında fark olmadığı hipotez sonucuyla ortaya çıkmıştır.
+
+
+# Adım 2: Elde ettiğiniz test sonuçlarına göre müşteriye tavsiyede bulununuz.
+
+#İki grup arasında peki bir fark olmadığı için müşteri iki tarafı da seçebilir.Fakat yinede tıklanma başına ödenen fiyat
+#değişeceği için bu orana bakılabilir, iki grup gözlemlenmeye devam edebilir.
